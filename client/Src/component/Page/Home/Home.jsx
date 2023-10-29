21cee2045cf82546894cc35016eda39a12435d87
shristi123-lakra <142797639+shristi123-lakra@users.noreply.github.com>
2023-10-29 20:33:47 +0530

    Add files via upload

---
+++ client/Src/component/Page/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../../App.css'
+import HomeMainbar from '../../Homemainbar/HomeMainbar'
+import LeftSidebar from '../../LeftSidebar/LeftSidebar.js'
+import RightSidebar from '../../RightSidebar/RightSidebar.js'
+
+const Home = ()=> {
+  return (
+    <div className='home-container-1'>
+    <LeftSidebar/>
+      <div className='home-container-2'>
+        <HomeMainbar/>
+        <RightSidebar/>
+       
+      
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
