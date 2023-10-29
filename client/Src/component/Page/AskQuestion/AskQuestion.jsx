be360be385d4a87ed943d2adfa29860aea2d00a6
shristi123-lakra <142797639+shristi123-lakra@users.noreply.github.com>
2023-10-29 20:30:41 +0530

    Add files via upload

---
+++ client/Src/component/Page/AskQuestion/AskQuestion.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+ const AskQuestion = () => {
+ 
+    return (
+    
+    <>
+    {
+       
+      <div>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore minus dignissimos voluptas exercitationem impedit aspernatur illo nobis officiis, odit porro reiciendis dolores deleniti nemo quisquam provident inventore ea voluptatibus nostrum.
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima consectetur molestiae eum, et culpa sequi laborum in nostrum ea numquam quam veritatis iure fuga. Consectetur, sed. Neque temporibus corrupti tempora.
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad culpa et blanditiis nesciunt natus suscipit vero similique sint cumque, voluptatibus fuga rem dolore veniam qui consequuntur quo voluptate. Eveniet, delectus?
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus sequi asperiores eos esse doloribus maiores inventore reprehenderit provident laborum. Corporis quia at beatae itaque! Officia maxime exercitationem necessitatibus id consequatur.
+</p>
+      </div>
+   }
+    
+    </>
+  )
+}
+export default AskQuestion
